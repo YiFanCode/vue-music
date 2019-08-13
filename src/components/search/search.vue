5d11c9212b0b8bbeff4df04a6b4bc270e0ff4a11
YiFan <YiFan_Code@163.com>
2019-08-13 10:31:35 +0800

    项目初始化、header组件、顶部组件、轮播图组件编写

---
+++ src/components/search/search.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="search">
+
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus" scoped>
+@import '~common/stylus/variable'
+@import '~common/stylus/mixin'
+
+</style>
---
+++ src/components/singer/singer.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="singer">
+
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus" scoped>
+@import '~common/stylus/variable'
+@import '~common/stylus/mixin'
+
+</style>
