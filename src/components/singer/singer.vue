97cca645384f8a244b8770a2667879089773d1c8
YiFan <YiFan_Code@163.com>
2019-08-13 16:12:05 +0800

    歌手数据处理&Singer类的封装

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -5,11 +5,87 @@
 </template>
 
 <script type="text/ecmascript-6">
-export default {}
+import { getSingerList } from 'api/singer'
+import { ERR_OK } from 'api/config'
+import Singer from 'common/js/singer'
+
+const HOT_NAME = '热门'
+const HOT_SINGER_LEN = 10
+
+export default {
+  data() {
+    return {
+      singers: []
+    }
+  },
+  mounted() {
+    this._getSingerList()
+  },
+  methods: {
+    async _getSingerList() {
+      const res = await getSingerList()
+      if (res.code === ERR_OK) {
+        this.singers = this._normalizeSinger(res.data.list)
+        // console.log(this._normalizeSinger(res.data.list))
+      }
+    },
+
+    _normalizeSinger(list) {
+      let map = {
+        hot: {
+          title: HOT_NAME,
+          items: []
+        }
+      }
+
+      list.forEach((item, index) => {
+        if (index < HOT_SINGER_LEN) {
+          map.hot.items.push(new Singer({
+            id: item.Fsinger_mid,
+            name: item.Fsinger_name
+          }))
+        }
+
+        const key = item.Findex
+        if (!map[key]) {
+          map[key] = {
+            title: key,
+            items: []
+          }
+        }
+        map[key].items.push(new Singer({
+          id: item.Fsinger_mid,
+          name: item.Fsinger_name
+        }))
+      })
+      // 需要得到有序列表，我们需要处理map数据
+      let hot = []
+      let ret = []
+      for (let key in map) {
+        let val = map[key]
+        if (val.title.match(/[a-zA-Z]/)) {
+          ret.push(val)
+        } else if (val.title === HOT_NAME) {
+          hot.push(val)
+        }
+      }
+      ret.sort((a, b) => {
+        return a.title.charCodeAt(0) - b.title.charCodeAt(0)
+      })
+      return hot.concat(ret)
+    }
+  }
+}
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus" scoped>
-@import '~common/stylus/variable'
-@import '~common/stylus/mixin'
+// @import '~common/stylus/variable'
+// @import '~common/stylus/mixin'
+
+.singer
+  width: 100%
+  position: fixed
+  top: 88px
+  bottom: 20px
 
 </style>
